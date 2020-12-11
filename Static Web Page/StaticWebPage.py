1c36babd989b3048d7c5860c341dd8a2d391e60d
Ping <44315881+Ping6666@users.noreply.github.com>
2020-12-11 20:56:33 +0800

    Update StaticWebPage.py

--- Static Web Page/StaticWebPage.py
+++ Static Web Page/StaticWebPage.py
@@ -3,6 +3,9 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 
+"""
+@author: Ping
+"""
 
 def getURLFromWebsite(url_, inputfiles_):
     page_count, url_count = 0, 0
